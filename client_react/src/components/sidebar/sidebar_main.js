7a496cec920b43abdbb48a9b6275ee698df77e1a
Dan Farrell <Dan.Farrell@vertexinc.com>
2018-12-17 21:15:20 -0500

    better

--- client_react/src/components/sidebar/sidebar_main.js
+++ client_react/src/components/sidebar/sidebar_main.js
@@ -14,10 +14,15 @@ class SidebarMain extends Component {
   }
 
   render() {
-    let classDashboard = "item";
-    let classAccount = "item";
-    let classPhoneNum = "item ";
-    let classApps = "item";
+    // let classDashboard = "item";
+    // let classAccount = "item";
+    // let classPhoneNum = "item ";
+    // let classApps = "item";
+    let classDashboard = "";
+    let classAccount = "";
+    let classPhoneNum = " ";
+    let classApps = "";
+    console.log("PROPS", this.props);
     switch (this.props.page) {
       case "dashboard":
         classDashboard += " active";
@@ -36,30 +41,32 @@ class SidebarMain extends Component {
     }
     return (
       <div className={"ui left vertical menu sidebar-left"} style={{ fontSize: "16px" }}>
-        <div className={classDashboard} style={{ boxDirection: "none" }}>
-          <Link to={"dashboard"} className={"sidebar-text"} id={`${classDashboard === "item" ? "" : "active-panel"}`}>
-            <i className="icon tachometer alternate side-icons" style={{ marginRight: "12px" }} />
-            Dashboard
-          </Link>
-        </div>
-        <div className={classAccount}>
-          <Link to={"account"} className={"sidebar-text"} id={`${classAccount === "item" ? "" : "active-panel"}`}>
-            <i className="icon sliders horizontal side-icons" style={{ marginRight: "12px" }} />
-            Account
-          </Link>
-        </div>
-        <div className={classPhoneNum}>
-          <Link to={"phone_num"} className={"sidebar-text"} id={`${classPhoneNum === "item" ? "" : "active-panel"}`}>
-            <i className="icon phone side-icons" style={{ marginRight: "12px" }} />
-            Phone Numbers
-          </Link>
-        </div>
-        <div className={classApps}>
-          <Link to={"apps"} className={"sidebar-text"} id={`${classApps === "item" ? "" : "active-panel"}`}>
-            <i className="icon th large side-icons" style={{ marginRight: "12px" }} />
-            Applications
-          </Link>
-        </div>
+        <ul>
+          <div className={classDashboard} style={{ boxDirection: "none" }}>
+            <Link to={"dashboard"} className={"sidebar-text"} id={`${classDashboard === "" ? "" : "active-panel"}`}>
+              <i className="icon tachometer alternate side-icons" style={{ marginRight: "12px" }} />
+              Dashboard
+            </Link>
+          </div>
+          <div className={classAccount}>
+            <Link to={"account"} className={"sidebar-text"} id={`${classAccount === "" ? "" : "active-panel"}`}>
+              <i className="icon sliders horizontal side-icons" style={{ marginRight: "12px" }} />
+              Account
+            </Link>
+          </div>
+          <div className={classPhoneNum}>
+            <Link to={"phone_num"} className={"sidebar-text"} id={`${classPhoneNum === "" ? "" : "active-panel"}`}>
+              <i className="icon phone side-icons" style={{ marginRight: "12px" }} />
+              Phone Numbers
+            </Link>
+          </div>
+          <div className={classApps}>
+            <Link to={"apps"} className={"sidebar-text"} id={`${classApps === "" ? "" : "active-panel"}`}>
+              <i className="icon th large side-icons" style={{ marginRight: "12px" }} />
+              Applications
+            </Link>
+          </div>
+        </ul>
       </div>
     );
   }
